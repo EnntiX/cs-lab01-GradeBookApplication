6ec174cbcd8fafbcacab574836b844fdfc572298
Maciej Knutelski <maciej.knutelski@microsoft.wsei.edu.pl>
2023-03-19 19:22:57 +0100

    Create Command Update

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,9 +40,23 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            if (parts[2] == "standard")
+            {
+                StandardGradeBook gradeBook = new StandardGradeBook(name);
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else if (parts[2] =="ranked")
+            {
+                RankedGradeBook gradeBook = new RankedGradeBook(name);
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradeBook);
+            }
+            else
+            {
+                Console.WriteLine(parts[2] + "is not a supported type of gradebook, please try again");
+                return;
+            }
         }
 
         public static void LoadCommand(string command)
